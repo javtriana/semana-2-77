ef046dde974a9f1c5d9e577115d7391dc01b381f
javtriana <ing.javtriana@gmail.com>
2020-12-04 14:30:54 -0500

    TeamCards

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <TeamCard> </TeamCard>
   </div>
 </template>
 
 <script>
+import TeamCard from "./components/TeamCard.vue";
 
 export default {
-  name: 'App',
-}
+  name: "App",
+  components: {
+    TeamCard,
+  },
+};
 </script>
-
